ab0358390eb9363c18e3567ee1a686516c54a3bf
Simon Zou <simon.zou@gmail.com>
2022-02-06 09:34:33 -0500

    separate files

--- starting_template/function_practice/app.js
+++ starting_template/function_practice/app.js
@@ -1,5 +1,33 @@
-window.onload = function() {
-    let number_boxes = 7 ;
-    addToList(number_boxes);
-    document.getElementById("display-number").innerHTML = " " + number_boxes;
-};
+
+function display_isPositive_tests(tuples) {
+    let ol = document.createElement("ol");
+    for (test_case of tuples) {
+        let li = document.createElement("li");
+        let status = "";
+        if (isPositive(test_case[0]) === test_case[1]) {
+            li.className = "test-passed";
+            status = "✅ PASSED"
+        } else {
+            li.className = "test-failed";
+            status = "❌ FAILED"
+        }
+        let common_part = "isPositive(" + test_case[0] + 
+        ") == ";
+
+        li.innerHTML = status + ": Expected " + common_part + test_case[1] + ". " + 
+            "Got " + common_part + isPositive(test_case[0]) + ". ";
+        ol.appendChild(li);
+    }
+    let div = document.getElementById("isPositive-tests");
+    div.appendChild(ol);
+}
+
+test_cases = [
+    [-7, false],
+    [8, true],
+    [0, false],
+    [-3.4, false],
+    [100001, true]
+]
+
+display_isPositive_tests(test_cases);
---
+++ starting_template/function_practice/functions.js
@@ -0,0 +1,14 @@
+function isPositive(input_number) {
+    // wrong implementation. Returns true for everything
+
+    return true;
+
+    // The correct answer for reference
+
+    // if (input_number > 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+}
+
